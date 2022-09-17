8624ef3b9522d09b464f0bd954c4fdefc490c242
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-09-17 09:08:48 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+const employeesDiv = document.getElementById('employees-div');
+const employeeInfo = {
+    name: '',
+    email: '',
+    phone: '',
+    image: '',
+    state: '',
+    dob: '',
+};
+
+    fetch('https://randomuser.me/api/?results=12&?inc=,name,location,email,picture,phone,dob')
+        .then(checkStatus)
+        .then((res) => res.json())
+        .then((data) => { 
+            console.log(data); 
+            for (let i = 0; i < 12; i++) {
+                employeeInfo.name = `${data.results[i].name.first} ${data.results[i].name.last}`;
+                employeeInfo.email = data.results[i].email;
+                employeeInfo.phone = data.results[i].phone;
+                employeeInfo.image = data.results[i].picture.medium;
+                employeeInfo.state = data.results[i].location.state;
+                employeeInfo.dob = data.results[i].dob.date.substring(0, 10);
+                createEmployeeDiv(employeeInfo.image, employeeInfo.name, employeeInfo.email, employeeInfo.state);
+            }
+        })
+        .catch(error => console.log('There is a problem with the api.', error));
+
+
+    console.log(employeeInfo);
+
+
+
+
+
+
+// Creates a div with emplyeeinfo and append
+function createEmployeeDiv(imageUrl, employeeName, emplyeeEmail, employeeState) {
+    const mainDiv = document.createElement('div');
+    mainDiv.className = 'employee';
+
+    const employeeImg = `<image class="employee-img" src="${imageUrl}" alt="profile image of ${employeeName}">`;
+
+    const infoDiv = document.createElement('div');
+    infoDiv.className = 'employee-info';
+
+    const employeeInfo = `
+    <p>${employeeName}</p>
+    <p>${emplyeeEmail}</p>
+    <p>${employeeState}</p>
+    `;
+
+    mainDiv.innerHTML = employeeImg;
+    mainDiv.appendChild(infoDiv);
+    infoDiv.innerHTML = employeeInfo;
+    employeesDiv.appendChild(mainDiv);
+}
+
+
+// Checks the status of the fetch request
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
